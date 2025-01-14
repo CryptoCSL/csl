798cc681e324a811c82b650959e8441795cc6555
CryptoCSL <162709172+CryptoCSL@users.noreply.github.com>
2025-01-14 17:27:51 +0200

    Update mobile-game.js

--- Game1-Panda Boom Run/js/mobile-game.js
+++ Game1-Panda Boom Run/js/mobile-game.js
@@ -6,7 +6,7 @@ if (window.screen.orientation && window.screen.orientation.lock) {
     // Lock the screen orientation to landscape
     window.screen.orientation.lock("landscape").catch((err) => {
         console.warn("Orientation lock failed:", err.message);
-        alert("Please rotate your device to landscape for the best experience.");
+        // alert("Please rotate your device to landscape for the best experience.");
     });
 } else {
     // Fallback for older browsers
